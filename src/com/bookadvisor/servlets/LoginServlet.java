a9a757f1dfddc8051e6c3fbc620263cf327d0f09
BookAdvisor <l.maria-tome@alumnos.upm.es>
2018-05-13 16:17:22 +0200

    Lucas: cleaned up code

--- src/com/bookadvisor/servlets/LoginServlet.java
+++ src/com/bookadvisor/servlets/LoginServlet.java
@@ -4,13 +4,9 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.apache.catalina.connector.Request;
 
 import com.bookadvisor.dao.LectorDAOImplementation;
 import com.bookadvisor.dao.LibreriaDAOImplementation;
